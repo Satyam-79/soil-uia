67d985c2b50be359bb49e3e4b265f26cd62cd3bd
Satyam-79 <satyamverma79@gmail.com>
2022-11-24 19:24:36 +0530

    weather list

--- machineModel/ml.py
+++ machineModel/ml.py
@@ -4,7 +4,6 @@ from sklearn.ensemble import RandomForestRegressor
 from machineModel.api import limiting
 from urllib.request import urlopen
 import json
-    
 
 
 def processImage(uploadedFilePath):
@@ -30,35 +29,49 @@ def processImage(uploadedFilePath):
             medianHSV.append([np.median(img[:, :, 0]), np.median(
                 img[:, :, 1]), np.median(img[:, :, 2])])
 
-    return  np.array(medianHSV)
+    return np.array(medianHSV)
 
-def weatherPrediction(weatherRegressor,w_sc):
-    url="https://api.weatherapi.com/v1/forecast.json?key=bc63089e69da4649b0571422222311&q=28.439422,77.508743&days=5"
-    parameters=['avgtemp_c','maxwind_kph','totalprecip_mm','avghumidity']
+
+def weatherPrediction(weatherRegressor, w_sc):
+    url = "https://api.weatherapi.com/v1/forecast.json?key=bc63089e69da4649b0571422222311&q=28.439422,77.508743&days=5"
+    parameters = ['avgtemp_c', 'maxwind_kph', 'totalprecip_mm', 'avghumidity']
 
     response = urlopen(url)
     data_json = json.loads(response.read())
-    weather=[]
-    icons=[]
+    weather = []
+    icons = []
     for i in range(5):
-      row=[]
-      for para in parameters:
-        row.append(data_json['forecast']['forecastday'][i]['day'][para])
-      icons.append(data_json['forecast']['forecastday'][i]['day']['condition']['icon'])
-      weather.append(row)
-      
-    prediction = weatherRegressor.predict(w_sc.transform(np.array(weather)))
-    prediction=prediction-1
-    print(prediction)
-    return weather,prediction,icons
+        row = []
+        for para in parameters:
+            row.append(data_json['forecast']['forecastday'][i]['day'][para])
+        icons.append(data_json['forecast']['forecastday']
+                     [i]['day']['condition']['icon'])
+        weather.append(row)
 
-def prediction_fun(hsvRegressor,weatherRegressor, sc,w_sc, uploadedFilePath):
+    prediction = weatherRegressor.predict(w_sc.transform(np.array(weather)))
+    prediction = prediction-1
+
+    # print(prediction)
+
+    return weather, prediction, icons
+
+
+def prediction_fun(hsvRegressor, weatherRegressor, sc, w_sc, uploadedFilePath):
     medianHSV = processImage(uploadedFilePath)
 
     hsvPrediction = hsvRegressor.predict(sc.transform(medianHSV))
 
     result = np.mean(hsvPrediction)
+
+    weather, w_Prediction, icons = weatherPrediction(weatherRegressor, w_sc)
+
+    data = result
+    moistureList=[]
+    moistureList.append(data)
     
-    weatherList=weatherPrediction(weatherRegressor,w_sc)
-    
-    return limiting(int(result))
+    for preds in w_Prediction:
+        data = preds*data
+        moistureList.append(data)
+    print(moistureList)
+    return limiting(int(result)),weather, icons,moistureList
+
