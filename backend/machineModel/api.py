b1b8fd3dbc25c93aa1b9d875b6b85e45fd3fbed6
Satyam-79 <satyamverma79@gmail.com>
2023-03-08 22:30:15 +0530

    backend

---
+++ backend/machineModel/api.py
@@ -0,0 +1,16 @@
+def greenRemove(img):
+    mask = cv2.inRange(img, (36, 25, 25), (86, 255,255))
+    iMask = mask<1
+    green = np.zeros_like(img, np.uint8)
+    green[iMask] = img[iMask]
+    return green
+
+def limiting(value):
+    if value <= 0:
+        return 0
+    elif value >= 100:
+        return 100
+    else:
+        return value
+    
+    
